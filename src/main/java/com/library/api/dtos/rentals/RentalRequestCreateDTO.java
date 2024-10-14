2997411f95b07daab06a54ef5a1b661ebfef2f27
jpoliveiradev <info1joaopaulo@gmail.com>
2024-10-14 20:09:31 -0300

    feat: add validation for rental creation

--- src/main/java/com/library/api/dtos/rentals/RentalRequestCreateDTO.java
+++ src/main/java/com/library/api/dtos/rentals/RentalRequestCreateDTO.java
@@ -1,5 +1,7 @@
 package com.library.api.dtos.rentals;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -8,4 +10,16 @@ public record RentalRequestCreateDTO(@NotNull(message = "Campo obrigatório") Lo
                                      @NotNull(message = "Campo obrigatório") Long customer_id,
                                      @NotNull(message = "Campo obrigatório") LocalDate rentalDate,
                                      @NotNull(message = "Campo obrigatório") LocalDate forecastDate) {
+
+    @JsonIgnore
+    @AssertTrue(message = "A data de previsão não pode ser anterior à data de aluguel")
+    public boolean isForecastDateValid() {
+        return forecastDate.isAfter(rentalDate) || forecastDate.isEqual(rentalDate);
+    }
+
+    @JsonIgnore
+    @AssertTrue(message = "A data de aluguel não pode ser anterior à data atual")
+    public boolean isRentalDateValid() {
+        return rentalDate.isAfter(LocalDate.now()) || rentalDate.isEqual(LocalDate.now());
+    }
 }
