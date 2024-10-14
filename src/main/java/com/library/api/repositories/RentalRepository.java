da2b2f383dbc79eca4e0cc331f4d1258a47f1ad0
jpoliveiradev <info1joaopaulo@gmail.com>
2024-10-14 20:54:45 -0300

    feat: validate deletion of book and customer if rentals exist

--- src/main/java/com/library/api/repositories/RentalRepository.java
+++ src/main/java/com/library/api/repositories/RentalRepository.java
@@ -4,4 +4,6 @@ import com.library.api.entities.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+    boolean existsByBookId(Long bookId);
+    boolean existsByCustomerId(Long customerId);
 }
